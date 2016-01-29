1442ff302409bba9f72d6c9e30c15a3c0b96b435
Miroslav Vitanov <mvvitanov@gmail.com>
2016-01-29 14:10:26 +0200

    Add docblocks

--- src/Form/FormBuilder.php
+++ src/Form/FormBuilder.php
@@ -8,8 +8,17 @@ use Administr\Form\Field\Text;
 
 class FormBuilder
 {
+    /**
+     * @var array
+     */
     private $fields = [];
 
+    /**
+     * Shortcut for easier definition of forms.
+     *
+     * @param Closure $definition
+     * @return $this
+     */
     public function define(Closure $definition)
     {
         $definition($this);
@@ -17,18 +26,37 @@ class FormBuilder
         return $this;
     }
 
+    /**
+     * Add a field to the form.
+     *
+     * @param AbstractType $field
+     */
     public function add(AbstractType $field)
     {
         $this->fields[] = $field;
     }
 
+    /**
+     * Add a text field.
+     *
+     * @param $fieldName
+     * @param $fieldLabel
+     * @param array $options
+     */
+    public function text($fieldName, $fieldLabel, $options = [])
     {
         $this->add(new Text($fieldName, $fieldLabel, $options));
     }
 
+    /**
+     * Basic rendering of the form.
+     *
+     * @return string
+     */
     public function render()
     {
         $form = '';
+
         foreach($this->fields as $field)
         {
             $form .= $field->render();

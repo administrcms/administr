78c99bb36878bea1c631667647ff4815c820393b
Miroslav Vitanov <mvvitanov@gmail.com>
2016-01-29 13:57:05 +0200

    Allow for simpler definition of the form

--- src/Form/FormBuilder.php
+++ src/Form/FormBuilder.php
@@ -3,13 +3,20 @@
 namespace Administr\Form;
 
 
+use Closure;
 use Administr\Form\Field\AbstractType;
 use Administr\Form\Field\Text;
 
 class FormBuilder
 {
     private $fields = [];
-    private $rules = [];
+
+    public function define(Closure $definition)
+    {
+        $definition($this);
+
+        return $this;
+    }
 
     public function add(AbstractType $field)
     {

e70823299c0340a38e6bd3207a31d79c2c347a30
Miroslav Vitanov <mvvitanov@gmail.com>
2016-02-02 23:13:21 +0200

    Add a bit of styling

--- src/Views/users/login.blade.php
+++ src/Views/users/login.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-    {!! $form->render() !!}
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-sm-6 col-xs-12 col-lg-push-6 col-sm-push-6">
+                {!! $form->render() !!}
+            </div>
+        </div>
+    </div>
 
 @stop

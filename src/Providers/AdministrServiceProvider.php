57a3001a3aad1e32a4096e5e2d164385378984cf
Miroslav Vitanov <mvvitanov@gmail.com>
2016-02-08 17:09:09 +0200

    Remove the translatable package

--- src/Providers/AdministrServiceProvider.php
+++ src/Providers/AdministrServiceProvider.php
@@ -8,7 +8,6 @@ use Administr\Localization\LocalizeFacade;
 use Administr\Localization\LocalizeMiddleware;
 use Administr\Localization\LocalizationServiceProvider;
 use Administr\Providers\SidebarServiceProvider as AdministrSidebarServiceProvider;
-use Dimsav\Translatable\TranslatableServiceProvider;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
@@ -30,7 +29,6 @@ class AdministrServiceProvider extends ServiceProvider
         AdministrSidebarServiceProvider::class,
         LocalizationServiceProvider::class,
         FlashServiceProvider::class,
-        TranslatableServiceProvider::class,
     ];
 
     private $facades = [

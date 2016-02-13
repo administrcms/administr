e746aea8fa8816d67bb8cc07b302bb0926d333a4
Miroslav Vitanov <mvvitanov@gmail.com>
2016-02-13 09:34:36 +0200

    Fix logo url

--- src/Views/layout/master.blade.php
+++ src/Views/layout/master.blade.php
@@ -26,7 +26,7 @@
     <header class="main-header">
 
         <!-- Logo -->
-        <a href="index2.html" class="logo">
+        <a href="{{ route('administr.dashboard.index') }}" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>Admini</b>str</span>
             <!-- logo for regular state and mobile devices -->

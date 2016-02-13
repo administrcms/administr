d478cbee09d4982b9c43d170e3eeb66a38dd031b
Miroslav Vitanov <mvvitanov@gmail.com>
2016-02-13 09:35:13 +0200

    Link to logout

--- src/Views/layout/master.blade.php
+++ src/Views/layout/master.blade.php
@@ -173,7 +173,7 @@
                                     <a href="#" class="btn btn-default btn-flat">Profile</a>
                                 </div>
                                 <div class="pull-right">
-                                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                    <a href="{{ route('administr.auth.logout') }}" class="btn btn-default btn-flat">Sign out</a>
                                 </div>
                             </li>
                         </ul>

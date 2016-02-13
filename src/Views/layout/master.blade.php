0bf4af49bc3ebf825e57feaa4b53458d755298c6
Miroslav Vitanov <mvvitanov@gmail.com>
2016-02-13 09:34:48 +0200

    Shorter version of the title

--- src/Views/layout/master.blade.php
+++ src/Views/layout/master.blade.php
@@ -28,7 +28,7 @@
         <!-- Logo -->
         <a href="{{ route('administr.dashboard.index') }}" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>Admini</b>str</span>
+            <span class="logo-mini"><b>A</b>str</span>
             <!-- logo for regular state and mobile devices -->
             <span class="logo-lg"><b>Admini</b>str</span>
         </a>

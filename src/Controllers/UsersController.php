40832f2a78ed63649116531e3a195cb4c6b9b48f
Miroslav Vitanov <mvvitanov@gmail.com>
2016-02-22 15:08:44 +0200

    Basic use of the listview

--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -4,13 +4,23 @@ namespace Administr\Controllers;
 
 
 use Administr\Forms\UserForm;
+use Administr\Listview\ListView;
+use App\Models\User;
 use Illuminate\Routing\Controller;
 
 class UsersController extends Controller
 {
-    public function index()
+    public function index(ListView $ls)
     {
-        return view('administr::users.index');
+        $data = User::all();
+        $ls
+            ->setDataSource($data)
+            ->define(function(ListView $l){
+                $l->text('id', 'ID');
+                $l->text('email', 'Email');
+            });
+
+        return view('administr::users.index', compact('ls'));
     }
 
     public function edit(UserForm $form, $id)
--- src/Views/users/index.blade.php
+++ src/Views/users/index.blade.php
@@ -1,4 +1,5 @@
 @extends('administr::layout.master')
 
 @section('content')
+    {!! $ls !!}
 @stop
